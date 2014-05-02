a1974ab66d1be9c67b614a755a2dbf0543fab56a
sebalo1314daw2 <sebalo1314daw2@gmail.com>
2014-05-02 20:31:05 +0200

    Treballant amb ordres Git i la branca01 v0.1

--- README.txt
+++ README.txt
@@ -1,1 +1,1 @@
-Treballant amb ordres Git a la branca00 v0.2
+Treballant amb ordres Git a la branca01 v0.1
--- src/prjava02/Prjava02.java
+++ src/prjava02/Prjava02.java
@@ -28,6 +28,7 @@ public class Prjava02 {
             System.out.println("Arquitecura OS: " + System.getProperty("os.arch"));
             System.out.println("Creació d'una branca del projecte prjava02");
             System.out.println("Afegint més codi a la branca00 del projecte prjava02");
+            System.out.println("Afegint codi a la branca01 del projecte prjava02");
         } catch (IOException e) {
             e.printStackTrace();
         }
